c8027adcf465c97838ab83262c55fafc44a21d88
Michael Lipscombe <mlipscombe@outlook.com>
2016-11-11 07:53:31 -0500

    add factory

---
+++ src/CircuitBreaker/CircuitBreakerStateStoreFactory.cs
@@ -0,0 +1,18 @@
+using System;
+using CircuitBreaker.Interfaces;
+
+namespace CircuitBreaker
+{
+   public class CircuitBreakerStateStoreFactory
+   {
+      public CircuitBreakerStateStoreFactory()
+      {
+
+      }
+
+      public static ICircuitBreakerStateStore GetCircuitBreakerStateStore()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
